20e5efdad8b1aec2cc97f1e18447e0f613c94111
mitulpatel9 <mitulpatel0703@gmail.com>
2025-02-27 13:44:59 +0530

    path

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -36,9 +36,9 @@ app.use('/api/auth', userRoutes);
 app.use('/api/mess', messageRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, './frontend/dist')));
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+        res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'));
     });
 }
 
